d057684c8d7f388dfcf36d4563e78e04d8badaec
root <root@PC-PF3M55E9.company.local>
2023-10-10 08:07:01 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -41,6 +41,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
     }
+    basket.insert(Fruit::Banana, 1);
+    basket.insert(Fruit::Pineapple, 1);
 }
 
 #[cfg(test)]
